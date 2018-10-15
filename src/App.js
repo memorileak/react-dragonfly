e6ad0fd2db48dc4e7765d4e5d08b9b035f9f46d1
TungNguyen <skyfall007nvt@gmail.com>
2018-10-15 13:37:11 +0700

    Initialize app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            coords: null
+        };
+        this._getCurrentCoords = this._getCurrentCoords.bind(this);
+    };
+
+    _getCurrentCoords() {
+        navigator.geolocation.getCurrentPosition((data) => {
+            this.setState({
+                coords: data.coords
+            });
+        }, (err) => {
+            console.error(err);
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <button
+                        id="location-button"
+                        onClick={this._getCurrentCoords}
+                    >
+                        Know your location
+                    </button>
+                    {
+                        this.state.coords !== null
+                        ? (
+                            <div id="result-display">
+                                <div>
+                                    Longitude: {this.state.coords.longitude}
+                                </div>
+                                <div>
+                                    Latitude: {this.state.coords.latitude}
+                                </div>
+                            </div>
+                        ) : null
+                    }
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;

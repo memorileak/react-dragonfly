a2acf270b18c0732a8efc22310da5fca3380e336
TungNguyen <skyfall007nvt@gmail.com>
2018-11-10 12:32:26 +0700

    Add feature: Fivedays Weather

--- src/utils/TimeUtils.js
+++ src/utils/TimeUtils.js
@@ -9,8 +9,16 @@ module.exports = (function () {
         'Sat',
     ];
     return {
-        getDayOfWeek: function(timestamp) {
+        getDayOfWeek: function(timestamp = Date.now()) {
             return DAYS[(new Date(timestamp)).getDay()];
+        },
+        getHours: function(timestamp = Date.now()) {
+            const hours = (new Date(timestamp)).getHours();
+            return hours < 10 ? `0${hours}` :  `${hours}`;
+        },
+        getMinutes: function(timestamp = Date.now()) {
+            const minutes = (new Date(timestamp)).getMinutes();
+            return minutes < 10 ? `0${minutes}` :  `${minutes}`;
         },
     };
 })();
